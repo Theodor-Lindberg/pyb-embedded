2ac087afe183d2283bc29f3a0086eea068295ad1
Theodor Lindberg <theodorlindberg.se@gmail.com>
2020-03-30 22:39:11 +0200

    Made USART driver base methods pure virtual

--- src/hal/UsartDriverBase.hpp
+++ src/hal/UsartDriverBase.hpp
@@ -8,13 +8,13 @@ class vector;
 class SerialDriverBase {
 	public:
 	virtual ~SerialDriverBase() = default;
-	virtual bool initialize(BAUDRATE baudrate, DIRECTION direction, DATAWIDTH datawidth, STOPBITS stopbits, PARITY parity, HWCONTROL hw_control, OVERSAMPLING over_sampling, IUsartHook* const usart_it_hook);
-	virtual bool de_init();
-	virtual void open();
-	virtual void close();
-	virtual void write(uint8_t byte);
-	virtual bool send(const uint8_t* data, uint32_t start, uint32_t end);
-	virtual bool send_async(const uint8_t* data, uint32_t start, uint32_t end);
-	virtual bool print(const uint8_t* str);
-	virtual uint8_t read();
+	virtual bool initialize(BAUDRATE baudrate, DIRECTION direction, DATAWIDTH datawidth, STOPBITS stopbits, PARITY parity, HWCONTROL hw_control, OVERSAMPLING over_sampling, IUsartHook* const usart_it_hook) = 0;
+	virtual bool de_init() = 0;
+	virtual void open() = 0;
+	virtual void close() = 0;
+	virtual void write(uint8_t byte) = 0;
+	virtual bool send(const uint8_t* data, uint32_t start, uint32_t end) = 0;
+	virtual bool send_async(const uint8_t* data, uint32_t start, uint32_t end) = 0;
+	virtual bool print(const uint8_t* str) = 0;
+	virtual uint8_t read() = 0;
 };
